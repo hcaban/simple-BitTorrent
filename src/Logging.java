6421a0006e213eab965b04609a60bb9286fe9345
g8t0rboi <g8t0rboispam@gmail.com>
2023-12-03 19:57:20 -0500

    Updated Logging functionality

--- src/Logging.java
+++ src/Logging.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Logging {
@@ -21,7 +22,19 @@ public class Logging {
 
     public synchronized void connenctionMade(int peerID1, int peerID2) {
 
-        output.write(getTime() + ": Peer " + peerID1 + " established connection to Peer " + peerID2 + ".");
+        output.write(getTime() + ": Peer " + peerID1 + " makes a connection to " + peerID2 + ".");
+
+    }
+
+    public synchronized void secondConnectionMade(int peerID1, int peerID2) {
+
+        output.write(getTime() + ": Peer " + peerID1 + " is connected from Peer " + peerID2);
+
+    }
+
+    public synchronized void changeofPreferredNieghbors(int peerID1, int[] neighborIDs) {
+
+        output.write(getTime() + ": Peer " + peerID1 + " has the preferred neigbors " + Arrays.toString(neighborIDs));
 
     }
 
@@ -37,43 +50,50 @@ public class Logging {
 
     }
 
-    public synchronized void neighborChoke(int peerID1, int peerID2 ) {
+    public synchronized void neighborChoke(int peerID1, int peerID2) {
 
         output.write(getTime() + ": Peer " + peerID1 +  " is choked Peer " + peerID2 + ".");
 
     }
 
-    public synchronized void have(int peerID1, int peerID2 ) {
+    public synchronized void have(int peerID1, int peerID2, int pieceIndex) {
 
-        output.write(getTime() + ": Peer " + peerID1 + " received 'have' message from Peer " + peerID2 + ".");
+        output.write(getTime() + ": Peer " + peerID1 + " received the 'have' message from Peer " + peerID2 + " for the piece " + pieceIndex + ".");
 
     }
 
     public synchronized void receivedInterested(int peerID1, int peerID2) {
 
-        output.write(getTime() + ": Peer " + peerID1 +  " has received the 'interested' message from Peer " + peerID2 + ".");
+        output.write(getTime() + ": Peer " + peerID1 +  " received the 'interested' message from Peer " + peerID2 + ".");
 
     }
 
     public synchronized void receivedNotInterested(int peerID1, int peerID2) {
 
-        output.write(getTime() + ": Peer " + peerID1 + " has received the 'not interested' message from Peer " + peerID2 + ".");
+        output.write(getTime() + ": Peer " + peerID1 + " received the 'not interested' message from Peer " + peerID2 + ".");
 
     }
 
     public synchronized void receiveRequest(int peerID1, int peerID2) {
 
-        output.write(getTime() + ": Peer " + peerID1 + " has received the 'request' message from Peer " + peerID2 + ".");
+        output.write(getTime() + ": Peer " + peerID1 + " received the 'request' message from Peer " + peerID2 + ".");
+
+    }
+
+    public synchronized void finishedDownloadPiece(int peerID1, int peerID2, int pieceIndex, int numPieces) {
+
+        output.write(getTime() + ": Peer " + peerID1 + " has finished downloading piece " + pieceIndex + " from Peer " + peerID2 + ". Now there are " + numPieces + ".");
 
     }
 
     public synchronized void finishedDownload(int peerID1) {
 
-        output.write(getTime() + ": Peer " + peerID1 + " has finished downloading the file.");
+        output.write(getTime() + ": Peer " + peerID1 + " has downloaded the complete file.");
 
     }
     public String getTime() {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy 'at' HH:mm:s z");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
         Date date = new Date(System.currentTimeMillis());
         return dateFormat.format(date);
 
